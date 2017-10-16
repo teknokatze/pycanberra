522a8c79a4e6024dfff2826ad83c8bc73ad913e5
ng0 <ng0@infotropique.org>
2017-10-16 19:45:08 +0000

    setup + README changes.
    First upstream is on sleep for years, I'm taking over maintenance (and probably extending this).

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(
     name='pycanberra',
-    version='0.1',
+    version='0.1.1',
     description='Ctypes wrapper for the libcanberra API',
-    author='Dave Barry',
+    author='ng0',
     url='https://ng0.git.infotropique.org/pycanberra/',
     license='LGPL-2.1',
     py_modules=['pycanberra']
