99ae78d4e39eabebbf076f03db98618cf53f58fb
Dmitri Bogomolov <4glitch@gmail.com>
2017-10-16 15:34:53 +0000

    Simple distutils setup script
    Signed-off-by: ng0 <ng0@infotropique.org>
    
    committer: ng0 <ng0@infotropique.org>

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pycanberra',
+    version='0.1',
+    description='A ctypes wrapper for the libcanberra api',
+    author='Dave Barry',
+    url='https://github.com/totdb/pycanberra',
+    py_modules=['pycanberra']
+)
