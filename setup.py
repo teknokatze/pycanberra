f6622eabac5c2beed824859c35c04ea60c66528a
ng0 <ng0@n0.is>
2017-11-28 17:11:26 +0000

    names.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.1',
     description='Ctypes wrapper for the libcanberra API',
     author='ng0',
-    url='https://ng0.git.infotropique.org/pycanberra/',
+    url='https://c.n0.is/pycanberra/',
     license='LGPL-2.1',
     py_modules=['pycanberra']
 )
